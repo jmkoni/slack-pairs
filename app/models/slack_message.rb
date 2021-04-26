571350928ff94214530a67cd530fa3c83eb17616
Jennifer Konikowski <jmkoni@icloud.com>
2021-04-26 09:20:59 -0400

    update introductions

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -2,13 +2,16 @@ module SlackMessage
   module_function
 
   def pair_message(pair:)
+    pair_usernames = ""
+    pair_usernames = " <@#{pair[0]}> and <@#{pair[1]}>" if pair.size == 2
+    pair_usernames = " <@#{pair[0]}>, <@#{pair[1]}>, and <@#{pair[2]}>" if pair.size == 3
     [
       {
         type: "section",
         block_id: "pair_introduction",
         text: {
           type: "mrkdwn",
-          text: ":wave: Hi <@#{pair[0]}> and <@#{pair[1]}>! You've both been paired up for a coffee chat from #coffee-buddies! Find a time to meet (Calendly is great for this) and have fun!"
+          text: ":wave: Hi#{pair_usernames}! You've both been paired up for a coffee chat from #coffee-buddies! Find a time to meet (Calendly is great for this) and have fun!"
         }
       }
     ]
