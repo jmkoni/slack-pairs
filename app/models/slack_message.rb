bd93f7817afee7c422b37b98d22b70b14b8ae398
Jennifer Konikowski <jmkoni@icloud.com>
2022-02-22 12:23:53 -0500

    Update slack_message.rb

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -9,7 +9,7 @@ module SlackMessage
         block_id: "pair_introduction",
         text: {
           type: "mrkdwn",
-          text: ":wave: Hi#{pair_usernames}! You've both been paired up for a coffee chat from #coffee-buddies! Find a time to meet (Calendly is great for this) and have fun!"
+          text: ":wave: Hi#{pair_usernames}! #{ENV["PAIR_MESSAGE"]}"
         }
       }
     ]
