aa235a12fc3470cea4bfa033923bd110d305e225
Jennifer Konikowski <jennifer.konikowski@splice.com>
2022-12-14 13:35:33 -0500

    fix home controller -> basic index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def index
+    render inline: "<p>Are you looking for <a href='https://jmkoni.github.io/slack-pairs/'>the docs</a>?</p>"
+  end
 end
