76c3de23475ce5082d13d680d9f6760209e72c84
Jennifer Konikowski <jmkoni@icloud.com>
2024-04-15 13:16:31 -0400

    only do both at the start of the month

--- spec/jobs/create_groups_job_spec.rb
+++ spec/jobs/create_groups_job_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe CreateGroupsJob do
   it "pairs people on first monday if pairing channel is set" do
     ENV["PAIRING_CHANNEL"] = "ABCD"
+    ENV["GROUPS_CHANNEL"] = ""
     allow(Date).to receive(:today).and_return(Date.new(2021, 6, 7))
     allow(Slack::Client).to receive(:get_channel_users).and_return((1..21).to_a)
     expect(Slack::Client).to receive(:create_conversation).exactly(10)
@@ -11,6 +12,7 @@ RSpec.describe CreateGroupsJob do
 
   it "doesn't group people on odd mondays if it's not the first monday" do
     ENV["PAIRING_CHANNEL"] = "ABCD"
+    ENV["GROUPS_CHANNEL"] = ""
     allow(Date).to receive(:today).and_return(Date.new(2021, 6, 21))
     expect(Slack::Client).to receive(:get_channel_users).exactly(0)
     expect(Slack::Client).to receive(:create_conversation).exactly(0)
@@ -18,8 +20,9 @@ RSpec.describe CreateGroupsJob do
   end
 
   it "groups people on first monday if groups channel is set" do
+    ENV["PAIRING_CHANNEL"] = ""
     ENV["GROUPS_CHANNEL"] = "ABCD"
-    allow(Date).to receive(:today).and_return(Date.new(2021, 6, 14))
+    allow(Date).to receive(:today).and_return(Date.new(2021, 6, 7))
     allow(Slack::Client).to receive(:get_channel_users).and_return((1..21).to_a)
     expect(Slack::Client).to receive(:create_conversation).exactly(6)
     CreateGroupsJob.perform
