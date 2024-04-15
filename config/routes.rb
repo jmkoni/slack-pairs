42878feb66f7ecf015f9e6488dad8e4f4b326bdc
Jennifer Konikowski <jmkoni@icloud.com>
2024-04-15 13:16:30 -0400

    fix home controller -> basic index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :mod, only: [:create]
-  root to: "home#index"
+  root to: "application#index"
 end
